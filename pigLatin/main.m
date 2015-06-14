76004dcbcad4e9f4bf2b993cef3a1e892de68ba3
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-13 20:28:55 -0400

    Initial Commit

---
+++ pigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  pigLatin
+//
+//  Created by Arsalan Vakili on 2015-06-13.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
